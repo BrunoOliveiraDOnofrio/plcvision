4c316a50478cdc15f335988116a0c35acbe20067
RanierDalton <ranier.couto@sptech.school>
2025-02-27 10:29:23 -0300

    COmplementar shell script com instalação do mysql

--- Scripts/configInstancia.sh
+++ Scripts/configInstancia.sh
@@ -7,5 +7,9 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install python3-pip
 
 # baixar as bibliotecas
-pip install psutil
-pip install mysql-connector-python
+pip install psutil==7.0.0
+pip install mysql-connector-python==9.2.0
+
+# instalando mysql
+sudo apt install mysql-server
+# a partir daq vou ter q testar na mão, por enquanto paro por aq
