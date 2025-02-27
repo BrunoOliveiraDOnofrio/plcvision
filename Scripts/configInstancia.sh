d42fdbacad5553f6819c9d1a1e89155af259faeb
RanierDalton <ranier.couto@sptech.school>
2025-02-27 13:29:46 -0300

    Adicionar configuração do projeto node e python

--- Scripts/configInstancia.sh
+++ Scripts/configInstancia.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 # att a maquina 
-sudo apt update && sudo apt upgrade -y
+echo "Atualizar Sistema"
+sudo apt -qq update && sudo apt -qq upgrade -y
+
+# instalar node 
+echo "Instalando NodeJS"
+sudo apt -qq install nodejs
 
 # instalar pip do python
-sudo apt install python3-pip
+echo "Instalando Pip"
+sudo apt -qq install python3-pip 
 
 # baixar as bibliotecas
-pip install psutil==7.0.0
-pip install mysql-connector-python==9.2.0
+echo "Instalando bibliotecas python necessárias"
+pip install psutil==7.0.0 --quiet
+pip install mysql-connector-python==9.2.0 --quiet
 
 # instalando mysql
-sudo apt install mysql-server
+echo "Instalando MYSQL Server"
+sudo apt -qq install mysql-server
+# TODO configurar e criar usuários necessários para rodar o banco
+
+# configurar projeto node
+echo "Configurando e inicializando web-data-viz"
+npm i && npm start
+
+# iniciar script python 
+echo "Inicializando script de captura"
+python3 ./Scripts/captura_dados.py
+
 # a partir daq vou ter q testar na mão, por enquanto paro por aq
