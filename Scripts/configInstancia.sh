585d091cb6e2424717ddc0ed3afd29460d31eb3c
RanierDalton <ranier.couto@sptech.school>
2025-02-28 18:14:40 -0300

    Adicionando configuração do MYSQL server

--- Scripts/configInstancia.sh
+++ Scripts/configInstancia.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
 
-# att a maquina 
-echo "Atualizar Sistema"
-sudo apt -y update && sudo apt -y upgrade
+# os q's são de quiet (silencio) para mostrar menos texto
+# e os y's são para não ficar fazendo perguntas no meio dos comandos
 
-# instalar node 
-echo "Instalando NodeJS"
-sudo apt -qq -y install nodejs
+# Atualizar a máquina
+echo "Atualizando Sistema..."
+sudo apt update -qq -y && sudo apt upgrade -qq -y
 
-# instalar pip do python
-echo "Instalando Pip"
-sudo apt -qq -y install python3-pip
+# Instalar Node.js 
+echo "Instalando NodeJS..."
+sudo apt install -qq -y nodejs npm
 
-# baixar as bibliotecas
-echo "Instalando bibliotecas python necessárias"
-pip install psutil==7.0.0 --quiet
-pip install mysql-connector-python==9.2.0 --quiet
+# Instalar pip do Python 
+echo "Instalando Pip..."
+sudo apt install -qq -y python3-pip
+
+# Instalar bibliotecas Python necessárias
+echo "Instalando bibliotecas Python..."
+pip install --quiet --no-input psutil==7.0.0 mysql-connector-python==9.2.0
 
 # instalando mysql
-echo "Instalando MYSQL Server"
+echo "Instalando MYSQL Server..."
 sudo apt -qq -y install mysql-server
-
 sudo systemctl start mysql.service
 
-sudo mysql src/database/modelagem.sql
+# configurando MYSQL
+echo "Configurando MYSQL..."
+sudo mysql < src/database/modelagem.sql
 
 CREATE USER 'plc_root'@'%' IDENTIFIED BY 'Urubu100';
-GRANT ALL PRIVILEGES ON PlcVision.* TO 'username'@'host';
+GRANT ALL PRIVILEGES ON PlcVision.* TO 'plc_root'@'%';
 FLUSH PRIVILEGES;
 
 CREATE USER 'insert_user'@'%' IDENTIFIED BY 'Urubu100';
-GRANT INSERT PRIVILEGE ON PlcVision.dados TO 'insert_user'@'%';
-GRANT INSERT PRIVILEGE ON PlcVision.alertas TO 'insert_user'@'%';
+GRANT INSERT ON PlcVision.dados TO 'insert_user'@'%';
+GRANT INSERT ON PlcVision.alertas TO 'insert_user'@'%';
 FLUSH PRIVILEGES;
 
 CREATE USER 'select_user'@'%' IDENTIFIED BY 'Urubu100#';
-GRANT PRIVILEGE ON PlcVision.* TO 'select_user'@'%';
+GRANT SELECT ON PlcVision.* TO 'select_user'@'%';
 FLUSH PRIVILEGES;
 
-# configurar projeto node
-echo "Configurando e inicializando web-data-viz"
+# configurar e rodar projeto node
+echo "Configurando e inicializando web-data-viz..."
 npm i && npm start
 
 # iniciar script python 
-echo "Inicializando script de captura"
+echo "Inicializando script de captura..."
 python3 ./Scripts/captura_dados.py
 
 # TODO a partir daq vou ter q testar na mão, por enquanto paro por aq
