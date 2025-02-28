3f67677254f95e76679aab7c455320d1649e9976
RanierDalton <ranier.couto@sptech.school>
2025-02-28 14:31:39 -0300

    Adicionar configuração inicial do mysql server

--- Scripts/configInstancia.sh
+++ Scripts/configInstancia.sh
@@ -2,15 +2,15 @@
 
 # att a maquina 
 echo "Atualizar Sistema"
-sudo apt update && sudo apt upgrade
+sudo apt -y update && sudo apt -y upgrade
 
 # instalar node 
 echo "Instalando NodeJS"
-sudo apt -qq install nodejs
+sudo apt -qq -y install nodejs
 
 # instalar pip do python
 echo "Instalando Pip"
-sudo apt -qq install python3-pip 
+sudo apt -qq -y install python3-pip
 
 # baixar as bibliotecas
 echo "Instalando bibliotecas python necessárias"
@@ -19,8 +19,24 @@ pip install mysql-connector-python==9.2.0 --quiet
 
 # instalando mysql
 echo "Instalando MYSQL Server"
-sudo apt -qq install mysql-server
-# TODO configurar e criar usuários necessários para rodar o banco
+sudo apt -qq -y install mysql-server
+
+sudo systemctl start mysql.service
+
+sudo mysql src/database/modelagem.sql
+
+CREATE USER 'plc_root'@'%' IDENTIFIED BY 'Urubu100';
+GRANT ALL PRIVILEGES ON PlcVision.* TO 'username'@'host';
+FLUSH PRIVILEGES;
+
+CREATE USER 'insert_user'@'%' IDENTIFIED BY 'Urubu100';
+GRANT INSERT PRIVILEGE ON PlcVision.dados TO 'insert_user'@'%';
+GRANT INSERT PRIVILEGE ON PlcVision.alertas TO 'insert_user'@'%';
+FLUSH PRIVILEGES;
+
+CREATE USER 'select_user'@'%' IDENTIFIED BY 'Urubu100#';
+GRANT PRIVILEGE ON PlcVision.* TO 'select_user'@'%';
+FLUSH PRIVILEGES;
 
 # configurar projeto node
 echo "Configurando e inicializando web-data-viz"
@@ -30,4 +46,4 @@ npm i && npm start
 echo "Inicializando script de captura"
 python3 ./Scripts/captura_dados.py
 
-# a partir daq vou ter q testar na mão, por enquanto paro por aq
+# TODO a partir daq vou ter q testar na mão, por enquanto paro por aq
