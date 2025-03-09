2be0d7e99c8777f487871c5f0b7a4fcf97081b74
RanierDalton <ranier.couto@sptech.school>
2025-03-09 12:02:33 -0300

    Modificar query's ao banco da autenticação e cadastro dos usuários e suas validações

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -5,80 +5,178 @@ function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
-
-        usuarioModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                                    res.json({
-                                        id: resultadoAutenticar[0].idUsuarios,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha
-                                    });
-                              
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+    
+    if(!validarCredenciais(res, email, senha)){
+        return;
     }
 
+    usuarioModel.autenticar(email, senha)
+    .then(
+        function (resultadoAutenticar) {
+            console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+            console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+            if (resultadoAutenticar.length == 1) {
+                console.log(resultadoAutenticar);
+
+                res.json({
+                    id: resultadoAutenticar[0].idUsuarios,
+                    nome: resultadoAutenticar[0].nome,
+                    nivel: resultadoAutenticar[0].nivel,
+                    fkEmpresa: resultadoAutenticar[0].fkEmpresa
+                });
+                        
+            } else if (resultadoAutenticar.length == 0) {
+                res.status(403).send("Email e/ou senha inválido(s)");
+            } else {
+                res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+            }
+        }
+    ).catch(
+        function (erro) {
+            console.log(erro);
+            console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        }
+    );
 }
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var telCelular = req.body.telCelularServer;
+    var nivel = req.body.nivelServer;
+    var setor = req.body.setorServer;
+    var cargo = req.body.cargoServer;
     var senha = req.body.senhaServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+    if (!validarCredenciais(res, email, senha)) {
+        return;
     }
+
+    if(!validarCadastro(res, nome, telCelular, nivel, setor, cargo, fkEmpresa)){
+        return;
+    }
+
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel.cadastrar(nome, email, telCelular, senha, nivel, setor, cargo, fkEmpresa)
+    .then(
+        function (resultado) {
+            res.json(resultado);
+        }
+    ).catch(
+        function (erro) {
+            console.log(erro);
+            console.log(
+                "\nHouve um erro ao realizar o cadastro! Erro: ",
+                erro.sqlMessage
+            );
+            res.status(500).json(erro.sqlMessage);
+        }
+    );
+}
+
+function validarCadastro(res, nome, telCelular, nivel, setor, cargo, fkEmpresa){
+    // nome completo
+    nome = nome.split(" ");
+    
+    if (nome == undefined || nome == '' || nome.length < 2) {
+        res.status(400).send("Nome completo inválido!");
+        return false;
+    } 
+    
+    // tel de DDD+numeros (13 digitos numericos)
+    if ((telCelular == undefined || telCelular == '') || telCelular.length != 13) {
+        res.status(400).send("Telefone Celular inválido!");
+        return false;
+    } 
+    
+    // 0 ou 1 ou 2
+    if ((nivel == undefined || nivel == '') || (nivel != 0 && nivel != 1 && nivel != 2)) {
+        res.status(400).send("Nível de acesso inválido!");
+        return false;
+    } 
+
+    // nada específico
+    if(setor == undefined || setor == ''){ 
+        res.status(400).send("Setor não definido!");
+        return false;
+    }
+
+    // nada específico
+    if (cargo == undefined || cargo == '') {   
+        res.status(400).send("Cargo não definido!");
+        return false;
+    }
+    
+    // nada específico tbm
+    if (fkEmpresa == undefined || fkEmpresa == '') {
+        res.status(400).send("Empresa não definida!");
+        return false;
+    }
+
+    return true;
+}
+
+function validarCredenciais(res, email, senha){
+    // aql validação de @ e final de email
+    var tamEmail = email.length;
+
+    var isEnd = email.endsWith('.com') || email.endsWith('.br') || email.endsWith('.gov');
+
+    // Encontrar o menor índice válido para '.com', '.br' ou '.gov'
+    var indices = [email.indexOf('.com'), email.indexOf('.br'), email.indexOf('.gov')].filter(i => i >= 0);
+    var indiceEnd = indices.length > 0 ? Math.min(...indices) : -1;
+
+    var indiceArroba = email.indexOf('@');
+    var isArroba = indiceArroba !== -1 && indiceArroba < indiceEnd;
+
+    if (email == undefined || email == '') {
+        res.status(400).send("Valor do email indefinido!");
+        return false;
+    }
+
+    if (tamEmail < 8) {
+        res.status(400).send("Tamnho do email é inválido!");
+        return false;
+    }
+
+    if(!isEnd || !isArroba){
+        res.status(400).send("Email inválido!");
+        return false;
+    }
+
+    var tamSenha = senha.length;
+    var isEspecial = CARACTERES_ESPECIAIS.test(senha);
+
+    var isMinuscula = false;
+    var isMaiuscula = false;
+    var isNum = false;        
+
+    // pd usar um forEach ou find
+    for(var i = 0; i < tamSenha; i++){
+        if(senha[i] == senha[i].toUpperCase()){
+            isMaiuscula = true;
+        }
+
+        if(senha[i] == senha[i].toLowerCase()){
+            isMinuscula = true;
+        }
+
+        if(Number(parseFloat(senha[i])) == senha[i]){
+            isNum = true;
+        }
+    }
+
+    if(tamSenha < 8 || !isMinuscula || !isMaiuscula || !isEspecial || !isNum || senha == ''){
+        res.status(400).send("Senha inválida!");
+        return false;
+    }
+
+    return true;
 }
 
 module.exports = {
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,22 +1,22 @@
 var database = require("../database/config")
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ")
     var instrucaoSql = `
-        SELECT idUsuarios, nome, email, fkEmpresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuarios, nome, nivel, fkEmpresa FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+// Coloque os mesmos parâmetros aqui. Vá para a instrucaoSql
+function cadastrar(nome, email, telCelular, senha, nivel, setor, cargo, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():");
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
+    const instrucaoSql = `
+        INSERT INTO usuario (nome, email, telCelular, senha, nivel, setor, cargo, fkEmpresa) VALUES ('${nome}', '${email}', '${telCelular}',  '${senha}', '${nivel}', '${setor}', '${cargo}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
