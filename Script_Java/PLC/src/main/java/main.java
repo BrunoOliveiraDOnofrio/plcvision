7cf07f990fe4bfc103d3ace5510d51f0bbd1f3d1
PAZanini <pedro.zanini@sptech.school>
2025-03-15 17:31:14 -0300

    Criando um metodo select sort para a Quantidade de ram

--- Script_Java/PLC/src/main/java/main.java
+++ Script_Java/PLC/src/main/java/main.java
@@ -9,5 +9,38 @@ public class main {
         plc[3] = new PLC("Rockwell", "ThinkPad X1", 32);
         plc[4] = new PLC("Simmens", "ZenBook", 16);
         plc[5] = new PLC("Rockwell", "ZenBook", 8);
+
+
+
+        System.out.println("Array PLC original");
+        for(int i=0; i < plc.length; i++) {
+            plc[i].exibir();
+        }
+
+
+        System.out.println("\nArray PLC organizado por quantidade de ram");
+        ramgbSort(plc);
+        for(int i=0; i < plc.length; i++) {
+            plc[i].exibir();
+        }
+
+    }
+
+    public static void ramgbSort(PLC[] plc){
+
+        for(int i=0; i < plc.length; i++){
+            int menor = i;
+            for(int j=i+1; j < plc.length; j++){
+                if(plc[j].ramgb < plc[menor].ramgb){
+                    menor = j;
+                }
+            }
+            PLC temp = plc[i];
+            plc[i] = plc[menor];
+            plc[menor] = temp;
+        }
+
+    }
+
     }
 }
