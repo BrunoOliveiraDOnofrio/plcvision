0935a942ea9f9cc24f45429b8ea2624080f2067b
PAZanini <pedro.zanini@sptech.school>
2025-03-15 17:55:21 -0300

    Criando um metodo select sort para todos os critérios

--- Script_Java/PLC/src/main/java/main.java
+++ Script_Java/PLC/src/main/java/main.java
@@ -5,38 +5,50 @@ public class main {
         PLC[] plc = new PLC[6];
         plc[0] = new PLC("Simmens", "XPS 13", 8);
         plc[1] = new PLC("Mitsubishi", "Spectre x360", 16);
-        plc[2] = new PLC("Simmens", "MacBook Air", 8);
+        plc[2] = new PLC("Simmens", "XPS 13", 8);
         plc[3] = new PLC("Rockwell", "ThinkPad X1", 32);
         plc[4] = new PLC("Simmens", "ZenBook", 16);
         plc[5] = new PLC("Rockwell", "ZenBook", 8);
 
 
-
         System.out.println("Array PLC original");
-        for(int i=0; i < plc.length; i++) {
+        for (int i = 0; i < plc.length; i++) {
             plc[i].exibir();
         }
 
 
         System.out.println("\nArray PLC organizado por quantidade de ram");
         ramgbSort(plc);
-        for(int i=0; i < plc.length; i++) {
+        for (int i = 0; i < plc.length; i++) {
             plc[i].exibir();
         }
 
         System.out.println("\nArray PLC organizado por marca");
         marcaSort(plc);
-        for(int i=0; i < plc.length; i++) {
+        for (int i = 0; i < plc.length; i++) {
+            plc[i].exibir();
+        }
+
+        System.out.println("\nArray PLC organizado por modelo");
+        modeloSort(plc);
+        for (int i = 0; i < plc.length; i++) {
+            plc[i].exibir();
+        }
+
+        System.out.println("\nArray PLC totalmente organizado");
+        sortTotal(plc);
+        for (int i = 0; i < plc.length; i++) {
             plc[i].exibir();
         }
     }
 
-    public static void ramgbSort(PLC[] plc){
 
-        for(int i=0; i < plc.length; i++){
+    public static void ramgbSort(PLC[] plc) {
+
+        for (int i = 0; i < plc.length; i++) {
             int menor = i;
-            for(int j=i+1; j < plc.length; j++){
-                if(plc[j].ramgb < plc[menor].ramgb){
+            for (int j = i + 1; j < plc.length; j++) {
+                if (plc[j].ramgb < plc[menor].ramgb) {
                     menor = j;
                 }
             }
@@ -47,12 +59,12 @@ public class main {
 
     }
 
-    public static void marcaSort(PLC[] plc){
+    public static void marcaSort(PLC[] plc) {
 
-        for(int i=0; i < plc.length; i++){
+        for (int i = 0; i < plc.length; i++) {
             int menor = i;
-            for(int j=i+1; j < plc.length; j++){
-                if(plc[j].marca.compareTo(plc[menor].marca) < 0){
+            for (int j = i + 1; j < plc.length; j++) {
+                if (plc[j].marca.compareTo(plc[menor].marca) < 0) {
                     menor = j;
                 }
             }
@@ -62,5 +74,49 @@ public class main {
         }
 
 
+    }
+
+
+    public static void modeloSort(PLC[] plc) {
+
+        for (int i = 0; i < plc.length; i++) {
+            int menor = i;
+            for (int j = i + 1; j < plc.length; j++) {
+                if (plc[j].modelo.compareTo(plc[menor].modelo) < 0) {
+                    menor = j;
+                }
+            }
+            PLC temp = plc[i];
+            plc[i] = plc[menor];
+            plc[menor] = temp;
+        }
+    }
+
+    public static void sortTotal(PLC[] plc) {
+        for (int i = 0; i < plc.length; i++) {
+            int menor = i;
+            for (int j = i + 1; j < plc.length; j++) {
+                int compararMarca = plc[j].marca.compareTo(plc[menor].marca);
+
+                if (compararMarca < 0) {
+                    menor = j;
+                } else if (compararMarca == 0) {
+                    int compararModelo = plc[j].modelo.compareTo(plc[menor].modelo);
+                    if (compararModelo < 0) {
+                        menor = j;
+                    }
+                    else if (compararModelo == 0) {
+                        if (plc[j].ramgb < plc[menor].ramgb) {
+                            menor = j;
+                        }
+                    }
+
+                }
+            }
+
+            PLC temp = plc[i];
+            plc[i] = plc[menor];
+            plc[menor] = temp;
+        }
     }
 }
