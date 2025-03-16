4d96a00567b8a5a52aca34eee89b11b895b22304
PAZanini <pedro.zanini@sptech.school>
2025-03-15 21:03:29 -0300

    Tratando dados do faker

--- Script_Java/PLC/src/main/java/main.java
+++ Script_Java/PLC/src/main/java/main.java
@@ -12,20 +12,28 @@ public class main {
 
 
         PLC[] plc = new PLC[6];
-        plc[0] = new PLC("Simmens", "XPS 13", 2008, 17, 18, 19, 8);
-        plc[1] = new PLC("Mitsubishi", "Spectre x360",2015, 3, 18, 11, 16);
-        plc[2] = new PLC("Simmens", "XPS 13",20019, 17, 90, 20, 16);
-        plc[3] = new PLC("Rockwell", "ThinkPad X1",2012, 17, 64, 15, 8);
-        plc[4] = new PLC("Simmens", "ZenBook",2022, 17, 18, 19, 8);
-        plc[5] = new PLC("Rockwell", "ZenBook",2021, 17, 18, 19, 32);
+//        plc[0] = new PLC("Simmens", "XPS 13", 2008, 17, 18, 19, 8);
+//        plc[1] = new PLC("Mitsubishi", "Spectre x360",2015, 3, 18, 11, 16);
+//        plc[2] = new PLC("Simmens", "XPS 13",20019, 17, 90, 20, 16);
+//        plc[3] = new PLC("Rockwell", "ThinkPad X1",2012, 17, 64, 15, 8);
+//        plc[4] = new PLC("Simmens", "ZenBook",2022, 17, 18, 19, 8);
+//        plc[5] = new PLC("Rockwell", "ZenBook",2021, 17, 18, 19, 32);
+
+        String[] array_marca = {"Simmens", "Mitsubishi", "Rockwell"};
+        String[] array_modelo = {"Spectre x360", "ZenBook", "XPS 13"};
+        Integer[] array_ram = {8, 16, 32};
 
 
-//        for (int i = 0; i < 6; i++) {
-//            Integer randon_numeros = faker.number().numberBetween(0, 99);
-//            String randon_palavra = faker.lorem().word();
-//            plc[i] = new PLC(randon_palavra, randon_palavra, randon_numeros, randon_numeros, randon_numeros, randon_numeros, randon_numeros);
-//
-//        }
+        for (int i = 0; i < 6; i++) {
+            Integer randon_numeros = faker.number().numberBetween(0, 99);
+            Integer modelo_randon = faker.number().numberBetween(0, 3);
+            Integer ram_randon = faker.number().numberBetween(0, 3);
+            Integer marca_randon = faker.number().numberBetween(0, 3);
+            Integer ano_randon = faker.number().numberBetween(2000, 2025);
+            String randon_palavra = faker.lorem().word();
+            plc[i] = new PLC(array_marca[marca_randon], array_modelo[modelo_randon], ano_randon, randon_numeros, randon_numeros, randon_numeros, array_ram[ram_randon]);
+
+        }
 
 
         System.out.println("Como voce gostaria de organizar seus plcs? \n 1. ram \n 2. Marca \n 3. Modelo \n 4. Usoram \n 5. Temperatura \n 6. Uso Cpu \n 7. Ano  \n 8. Tudo \n 9. Sair");
